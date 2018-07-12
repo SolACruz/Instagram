00553391f14c9ba18e48f773d074b3ac586fc89d
Sol Cruz <solacruz@fb.com>
2018-07-11 17:42:16 -0700

    added adapter, recycler view, etc, basically started trying to make home screen

--- app/src/main/java/me/solacruz/instagram/HomeActivity.java
+++ app/src/main/java/me/solacruz/instagram/HomeActivity.java
@@ -1,18 +1,36 @@
 package me.solacruz.instagram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import me.solacruz.instagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
+
+    //creates a reference to the client
+    //private TwitterClient client;
+    PostAdapter postAdapter;
+    ArrayList<Post> Posts;
+    RecyclerView rvPosts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +58,101 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.compose, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.Compose:
+                composeMessage();
+                return true;
+//            case R.id.miProfile:
+//                showProfileView();
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode==RESULT_OK_CODE && requestCode==REQUEST_CODE){
+            Tweet tweet= Parcels.unwrap(data.getParcelableExtra(Tweet.class.getSimpleName()));
+            tweets.add(0, tweet);
+            tweetAdaptor.notifyItemInserted(0);
+            rvTweets.scrollToPosition(0);
+            Toast.makeText(this, "Tweet Posted!", Toast.LENGTH_LONG);
+        }
+
+        // Use data parameter
+        //Tweet tweet = (Tweet) data.getSerializableExtra("tweet");
+    }
+
+    private final int REQUEST_CODE = 20;
+    private final int RESULT_OK_CODE=20;
+    // FirstActivity, launching an activity for a result
+    public void composeMessage() {
+        Intent i = new Intent(this, ComposeTweet.class);
+        i.putExtra("mode", 2); // pass arbitrary data to launched activity
+        startActivityForResult(i, REQUEST_CODE);
+    }
+
+
+    private void populateHome(){
+        client.getHomeTimeline(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                //Log.d("TwitterClient", response.toString());
+                //iterate through the JSON array
+                //for each entry, deserialize the JSON object
+                for(int i=0; i<response.length(); i++) {
+                    //convert each object into the tweet model
+
+                    //add the tweet model to the data source
+
+                    //notify adapter that an item was added
+                    Tweet tweet= null;
+                    try {
+                        tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdaptor.notifyItemInserted(tweets.size()-1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("TwitterClient", response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+        });
     }
 }
--- app/src/main/java/me/solacruz/instagram/PostAdapter.java
+++ app/src/main/java/me/solacruz/instagram/PostAdapter.java
@@ -1,6 +1,110 @@
 package me.solacruz.instagram;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PostAdapter extends RecyclerView{
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import me.solacruz.instagram.model.Post;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+    private List<Post> mPost;
+    Context context;
+    //pass in the tweets array into the constructor
+    public PostAdapter(List<Post> posts){
+        mPost=posts;
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+
+    //for each row, inflate the layout and cache references in the ViewHolder
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context= parent.getContext();
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View tweetView= inflater.inflate(R.layout.item_post, parent, false);
+        ViewHolder viewHolder= new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+
+    //bind the values based on the position of the element in the row view
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        //get the data according to its position
+        Post post=mPost.get(position);
+
+        //give the views the data in the correct order/populate the views
+        holder.tvUsername.setText(post.getUser().toString());
+        holder.tvDescription.setText(post.getDescription());
+        //holder.tvRelDate.setText(getRelativeTimeAgo(tweet.createdAt));
+
+        //Glide.with(context).load(tweet.user.profileImageURL).into(holder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    //create the viewHolder class
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView ivUserImage;
+        public TextView tvUsername;
+        public TextView tvDescription;
+        public TextView tvRelDate;
+
+
+        public ViewHolder (View itemView){
+            super(itemView);
+
+            //Find view by id
+            ivUserImage=itemView.findViewById(R.id.ivUserImage);
+            tvUsername=itemView.findViewById(R.id.tvUsername);
+            tvDescription=itemView.findViewById(R.id.tvDescription);
+            //tvRelDate=itemView.findViewById(R.id.tvRelDate);
+        }
+
+    }
+
+    public void clear(){
+        mPost.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Post> posts){
+        mPost.addAll(posts);
+        notifyDataSetChanged();
+    }
+
 }
