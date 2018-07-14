f7d2a297a654b3948dd51f356c17d062ef961718
Sol Cruz <solacruz@fb.com>
2018-07-13 20:02:07 -0700

    donee

--- app/src/main/java/me/solacruz/instagram/DetailedPostActivity.java
+++ app/src/main/java/me/solacruz/instagram/DetailedPostActivity.java
@@ -1,13 +1,55 @@
 package me.solacruz.instagram;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+
+import me.solacruz.instagram.model.Post;
 
 public class DetailedPostActivity extends AppCompatActivity {
+
+    private ImageView userImage;
+    private TextView username;
+    private TextView date;
+    private TextView description;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_post);
+
+        String postId=getIntent().getStringExtra("post");
+
+        userImage=findViewById(R.id.ivUserImage2);
+        username=findViewById(R.id.tvUsername2);
+        date=findViewById(R.id.tvDate2);
+        description=findViewById(R.id.tvDescription2);
+
+        Post.Query query=new Post.Query();
+
+        query.withUser();
+
+        query.getInBackground(postId, new GetCallback<Post>() {
+            @Override
+            public void done(Post object, ParseException e) {
+                if(e==null){
+                    username.setText(object.getUser().getUsername());
+                    date.setText(object.getCreatedAt().toString());
+                    description.setText(object.getDescription());
+                    Glide.with(DetailedPostActivity.this).load(object.getImage().getUrl()).into(userImage);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/me/solacruz/instagram/PostAdapter.java
+++ app/src/main/java/me/solacruz/instagram/PostAdapter.java
@@ -1,6 +1,7 @@
 package me.solacruz.instagram;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
@@ -22,28 +23,29 @@ import me.solacruz.instagram.model.Post;
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     private List<Post> mPost;
     Context context;
+    Post post;
     //pass in the tweets array into the constructor
     public PostAdapter(List<Post> posts){
         mPost=posts;
     }
 
-//    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
-//    public String getRelativeTimeAgo(String rawJsonDate) {
-//        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-//        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-//        sf.setLenient(true);
-//
-//        String relativeDate = "";
-//        try {
-//            long dateMillis = sf.parse(rawJsonDate).getTime();
-//            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-//                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return relativeDate;
-//    }
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
 
     //for each row, inflate the layout and cache references in the ViewHolder
 
@@ -64,12 +66,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     public void onBindViewHolder(ViewHolder holder, int position) {
 
         //get the data according to its position
-        Post post=mPost.get(position);
+        post=mPost.get(position);
 
         //give the views the data in the correct order/populate the views
         holder.tvUsername.setText(post.getUser().getUsername());
         holder.tvDescription.setText(post.getDescription());
-        //holder.tvRelDate.setText(getRelativeTimeAgo(tweet.createdAt));
+        holder.tvDate.setText(post.getCreatedAt().toString());
 
         Glide.with(context).load(post.getImage().getUrl()).into(holder.ivUserImage);
     }
@@ -80,11 +82,11 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     }
 
     //create the viewHolder class
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public ImageView ivUserImage;
         public TextView tvUsername;
         public TextView tvDescription;
-        //public TextView tvRelDate;
+        public TextView tvDate;
 
 
         public ViewHolder (View itemView){
@@ -94,9 +96,19 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
             ivUserImage=itemView.findViewById(R.id.ivUserImage);
             tvUsername=itemView.findViewById(R.id.tvUsername);
             tvDescription=itemView.findViewById(R.id.tvDescription);
-            //tvRelDate=itemView.findViewById(R.id.tvRelDate);
+            tvDate=itemView.findViewById(R.id.tvDate);
+
+            itemView.setOnClickListener(this);
+
         }
 
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(context, DetailedPostActivity.class);
+            String object=post.getObjectId();
+            intent.putExtra("post", object);
+            context.startActivity(intent);
+        }
     }
 
     public void clear(){
